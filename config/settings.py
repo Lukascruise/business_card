e9927a2b680e3b8895ec80e15e8b03c19ee019c5
lukascruise <rmeo4692@gmail.com>
2026-01-15 17:23:32 +0900

    chore(#01): chore: add pre-commit & ruff configuration
    chore: fix ruff lint errors
    
    chore: ignore django autogenerated E501

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+# ruff: noqa: E501
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
